ed0353ab3cf2e27d7ded606bd956033849e44501
harshitthummar <itsharshitthumar@gmail.com>
2023-11-10 16:18:56 +0530

    added animations of player

---
+++ Assets/Scripts/playerAnimator.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAnimator : MonoBehaviour
+{
+    [SerializeField] private Player player;
+    private const string IS_WALKING = "IsWalking";
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        animator.SetBool(IS_WALKING, player.IsWalking());
+    }
+}
